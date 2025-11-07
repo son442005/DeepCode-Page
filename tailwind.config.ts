e46b83331490558cc80e967fa1d2f4b6eb04465f
son442005 <letruongson442005@gmail.com>
2025-11-07 08:10:12 +0700

    fix-font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,31 @@ export default {
                     secondary: '#0EA5E9',
                     dark: '#0B1220',
                     rbg: '#B7B7B7',
+                },
+                fontFamily: {
+                    sans: [
+                        'Inter',
+                        'ui-sans-serif',
+                        'system-ui',
+                        '-apple-system',
+                        'Segoe UI',
+                        'Roboto',
+                        'Noto Sans',
+                        'Helvetica Neue',
+                        'Arial',
+                        'sans-serif',
+                        'Apple Color Emoji',
+                        'Segoe UI Emoji',
+                        'Segoe UI Symbol',
+                    ],
+                    serif: [
+                        'Noto Serif',
+                        'Georgia',
+                        'Cambria',
+                        'Times New Roman',
+                        'Times',
+                        'serif',
+                    ],
                 }
             }
     },
